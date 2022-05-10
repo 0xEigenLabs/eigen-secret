6c702eaded6c1d3d2664844726e73ebad2cd935e
JangoCCC <zebron@ieigen.com>
2022-05-10 13:30:07 +0800

    update script

--- scripts/bump_update_state_solidity.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-const plonkVerifierRegex = /contract KeyedVerifier/
-
-let content = fs.readFileSync("../contracts/zkit_update_state_verifier.sol", { encoding: 'utf-8' });
-let bumped = content.replace(plonkVerifierRegex, 'contract UpdateStateKeyedVerifier');
-
-fs.writeFileSync("../contracts/zkit_update_state_verifier.sol", bumped);
