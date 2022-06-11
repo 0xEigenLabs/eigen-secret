94fbe60a0f7b0c9ea32d14165e67ce8267cf446f
JangoCCC <zebron@ieigen.com>
2022-06-12 02:25:20 +0800

    feat: prover generates proof and verify

--- operator/config.ts
+++ operator/config.ts
@@ -2,7 +2,7 @@
 const _ = require("lodash");
 
 // module variables
-const config = require("../config.json");
+const config = require("../config/config.json");
 const defaultConfig = config.development;
 const environment = process.env.NODE_ENV || "development";
 const environmentConfig = config[environment];
