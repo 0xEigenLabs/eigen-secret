75fef9a5fb763c2dead2507b8be24a8b2e26f268
eigmax <stephen@ieigen.com>
2022-06-17 23:46:00 +0800

    feat: expose interfaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        '@typescript-eslint/no-var-requires': 0
-    }
-}
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'rules': {
+  },
+};
