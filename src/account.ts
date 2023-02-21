3c87733b9cd631b5a6f5317d752d557a9eb473c2
eigmax <stephen@ieigen.com>
2023-02-21 08:51:16 +0800

    chore: dev transaction

--- src/account.ts
+++ src/account.ts
@@ -1,8 +1,8 @@
 import { ethers } from "ethers";
 const buildEddsa = require("circomlibjs").buildEddsa;
-const buildBabyjub = require("circomlibjs").buildBabyjub;
 const { Scalar, utils } = require("ffjavascript");
 const createBlakeHash = require("blake-hash");
+const { Buffer } = require("buffer");
 
 interface Address {
     protocol: string;
@@ -19,38 +19,56 @@ export class EigenAddress implements Address {
         let p1 = BigInt(pubKey.substring(32));
         this.pubKey = [p0, p1];
     }
-};
-
-export interface Key {
-    prvKey: Uint8Array;
-    pubKey: [bigint, bigint] | ethers.utils.Bytes;
-    async new(seed: string): Promise<Key>;
 }
 
-// eddsa
-export class SigningKey implements Key {
-    prvKey: Uint8Array;
+type NewKeyFunc = (seed: string) => Promise<IKey>;
+export interface IKey {
+    prvKey: bigint;
     pubKey: [bigint, bigint] | ethers.utils.Bytes;
-    constructor() {}
-    async new(seed: string): Promise<Key> {
+    keyFunc: NewKeyFunc;
+}
+
+
+// eddsa
+export class SigningKey implements IKey {
+    prvKey: bigint = 0n;
+    pubKey: [bigint, bigint] | ethers.utils.Bytes = [0n, 0n];
+    constructor(prvKey: any, pubKey: any) {
+        this.prvKey = prvKey;
+        this.pubKey = pubKey;
+    }
+    keyFunc: NewKeyFunc = async (seed: string) => {
         let rawpvk = Buffer.from(ethers.utils.randomBytes(31));
         let eddsa = await buildEddsa();
         let pvk = createBlakeHash("blake512")
             .update(rawpvk)
             .update(seed)
             .digest().slice(0, 32);
-        this.prvKey = Scalar.shr(utils.leBuff2int(pvk), 3);
-        this.pubKey = eddsa.prv2pub(this.prvKey);
+        let prvKey = Scalar.shr(utils.leBuff2int(pvk), 3);
+        let pubKey = eddsa.prv2pub(prvKey);
+        let result: IKey = new SigningKey(prvKey, pubKey);
+        return new Promise<IKey>((resolve, reject) => {
+            resolve(result);
+        });
     }
 }
 
 // ecdsa
-export class AccountOrNullifierKey implements Key {
-    constructor() {}
+export class AccountOrNullifierKey implements IKey {
+    prvKey: bigint = 0n;
+    pubKey: [bigint, bigint] | ethers.utils.Bytes = [0n, 0n];
+    constructor(prvKey: any, pubKey: any) {
+        this.prvKey = prvKey;
+        this.pubKey = pubKey;
+    }
     // signature hex string
-    async new(signature: string): Promise<Key> {
-        // get the first 32bytes as accout key
-        this.prvKey = ethers.utils.arrayify(signature).slice(0, 32);
-        this.pubKey = ethers.utils.computePublicKey(this.prvKey);
+    keyFunc: NewKeyFunc = async (signature: string) => {
+        // return the first 32bytes as account key
+        let prvKey = ethers.utils.arrayify(signature).slice(0, 32);
+        let pubKey = ethers.utils.arrayify(ethers.utils.computePublicKey(prvKey));
+        let result: IKey = new AccountOrNullifierKey(prvKey, pubKey);
+        return new Promise<IKey>((resolve, reject) => {
+            resolve(result);
+        });
     }
 }
