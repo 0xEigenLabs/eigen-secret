7cf4dbe6127c4b0326494e4ef8e95b9ed3fe043d
ibmp33 <2285673866@qq.com>
2023-03-17 16:45:07 +0800

    chore: test

--- create_proof/createProof.ts
+++ create_proof/createProof.ts
@@ -7,9 +7,9 @@ const snarkjs = require("snarkjs");
 export class createProof{
 
     static async updateState(circuitPath: string, input: any, F: any) {
-        let wasm = path.join(circuitPath, "main_update_state_js", "main_update_state.wasm");
+        let wasm = path.join(circuitPath, "main_js", "main_update_state.wasm");
         let zkey = path.join(circuitPath, "circuit_final.zkey");
-        const wc = require(`${circuitPath}/main_update_state_js/witness_calculator`);
+        const wc = require(`${circuitPath}/main_js/witness_calculator`);
         const buffer = fs.readFileSync(wasm);
         const witnessCalculator = await wc(buffer);
     
