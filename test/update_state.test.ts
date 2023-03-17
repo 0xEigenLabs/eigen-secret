12d5d54418b20daef541a9899a42e257ec568be9
ibmp33 <2285673866@qq.com>
2023-03-17 16:37:29 +0800

    chore: forgot to restore some code

--- test/update_state.test.ts
+++ test/update_state.test.ts
@@ -42,109 +42,109 @@ describe("Test JoinSplit Circuit", function () {
         await worldState.init();
     })
 
-    // it("Account create update_state test", async () => {
-    //     let proofId = UpdateStatusCircuit.PROOF_ID_TYPE_CREATE;
-    //     let newAccountKey = accountKey;
-    //     let newAccountPubKey = newAccountKey.pubKey.unpack(babyJub);
-    //     newAccountPubKey = [F.toObject(newAccountPubKey[0]), F.toObject(newAccountPubKey[1])];
+    it("Account create update_state test", async () => {
+        let proofId = UpdateStatusCircuit.PROOF_ID_TYPE_CREATE;
+        let newAccountKey = accountKey;
+        let newAccountPubKey = newAccountKey.pubKey.unpack(babyJub);
+        newAccountPubKey = [F.toObject(newAccountPubKey[0]), F.toObject(newAccountPubKey[1])];
 
-    //     let newSigningKey1 = await (new SigningKey()).newKey(undefined);
-    //     let newSigningPubKey1 = newSigningKey1.pubKey.unpack(babyJub);
-    //     newSigningPubKey1 = [F.toObject(newSigningPubKey1[0]), F.toObject(newSigningPubKey1[1])];
+        let newSigningKey1 = await (new SigningKey()).newKey(undefined);
+        let newSigningPubKey1 = newSigningKey1.pubKey.unpack(babyJub);
+        newSigningPubKey1 = [F.toObject(newSigningPubKey1[0]), F.toObject(newSigningPubKey1[1])];
 
-    //     let newSigningKey2 = await (new SigningKey()).newKey(undefined);
-    //     let newSigningPubKey2 = newSigningKey2.pubKey.unpack(babyJub);
-    //     newSigningPubKey2 = [F.toObject(newSigningPubKey2[0]), F.toObject(newSigningPubKey2[1])];
+        let newSigningKey2 = await (new SigningKey()).newKey(undefined);
+        let newSigningPubKey2 = newSigningKey2.pubKey.unpack(babyJub);
+        newSigningPubKey2 = [F.toObject(newSigningPubKey2[0]), F.toObject(newSigningPubKey2[1])];
 
-    //     let input = await UpdateStatusCircuit.createAccountInput(
-    //         proofId,
-    //         accountKey,
-    //         signingKey,
-    //         newAccountPubKey,
-    //         newSigningPubKey1,
-    //         newSigningPubKey2,
-    //         aliasHash,
-    //         worldState
-    //     );
-    //     await utils.executeCircuit(circuit, input.toCircuitInput(babyJub));
+        let input = await UpdateStatusCircuit.createAccountInput(
+            proofId,
+            accountKey,
+            signingKey,
+            newAccountPubKey,
+            newSigningPubKey1,
+            newSigningPubKey2,
+            aliasHash,
+            worldState
+        );
+        await utils.executeCircuit(circuit, input.toCircuitInput(babyJub));
 
-    //     proofId = UpdateStatusCircuit.PROOF_ID_TYPE_MIGRATE;
-    //     newAccountKey = await (new SigningKey()).newKey(undefined);
-    //     newAccountPubKey = newAccountKey.pubKey.unpack(babyJub);
-    //     newAccountPubKey = [F.toObject(newAccountPubKey[0]), F.toObject(newAccountPubKey[1])];
+        proofId = UpdateStatusCircuit.PROOF_ID_TYPE_MIGRATE;
+        newAccountKey = await (new SigningKey()).newKey(undefined);
+        newAccountPubKey = newAccountKey.pubKey.unpack(babyJub);
+        newAccountPubKey = [F.toObject(newAccountPubKey[0]), F.toObject(newAccountPubKey[1])];
 
-    //     newSigningKey2 = await (new SigningKey()).newKey(undefined);
-    //     newSigningPubKey2 = newSigningKey2.pubKey.unpack(babyJub);
-    //     newSigningPubKey2 = [F.toObject(newSigningPubKey2[0]), F.toObject(newSigningPubKey2[1])];
-    //     input = await UpdateStatusCircuit.createAccountInput(
-    //         proofId,
-    //         accountKey,
-    //         signingKey,
-    //         newAccountPubKey,
-    //         newSigningPubKey1,
-    //         newSigningPubKey2,
-    //         aliasHash,
-    //         worldState
-    //     );
-    //     await utils.executeCircuit(circuit, input.toCircuitInput(babyJub));
-    // })
+        newSigningKey2 = await (new SigningKey()).newKey(undefined);
+        newSigningPubKey2 = newSigningKey2.pubKey.unpack(babyJub);
+        newSigningPubKey2 = [F.toObject(newSigningPubKey2[0]), F.toObject(newSigningPubKey2[1])];
+        input = await UpdateStatusCircuit.createAccountInput(
+            proofId,
+            accountKey,
+            signingKey,
+            newAccountPubKey,
+            newSigningPubKey1,
+            newSigningPubKey2,
+            aliasHash,
+            worldState
+        );
+        await utils.executeCircuit(circuit, input.toCircuitInput(babyJub));
+    })
 
-    // it("JoinSplit deposit and send update_state test", async () => {
-    //     signer = accountRequired? signingKey: accountKey;
-    //     acStateKey = await accountCompress(eddsa, accountKey, signer, aliasHash);
-    //     await worldState.insert(F.e(acStateKey), 1);
-    //     let proofId = UpdateStatusCircuit.PROOF_ID_TYPE_DEPOSIT;
-    //     let inputs = await UpdateStatusCircuit.createJoinSplitInput(
-    //         accountKey,
-    //         signingKey,
-    //         worldState,
-    //         acStateKey,
-    //         proofId,
-    //         aliasHash,
-    //         assetId,
-    //         assetId,
-    //         10n,
-    //         signingKey.pubKey,
-    //         10n,
-    //         accountKey.pubKey,
-    //         [],
-    //         accountRequired
-    //     );
-    //     for (const input of inputs) {
-    //         await utils.executeCircuit(circuit, input.toCircuitInput(babyJub));
-    //     }
-    //     console.log("test send tx")
-    //     let confirmedNote: Note[] = [];
-    //     for (const inp of inputs) {
-    //         inp.outputNotes[0].index = 10; // FIXME update index
-    //         inp.outputNotes[1].index = 10;
-    //         confirmedNote.push(inp.outputNotes[0]); // after depositing, all balance becomes private value
-    //         confirmedNote.push(inp.outputNotes[1]);
-    //     }
+    it("JoinSplit deposit and send update_state test", async () => {
+        signer = accountRequired? signingKey: accountKey;
+        acStateKey = await accountCompress(eddsa, accountKey, signer, aliasHash);
+        await worldState.insert(F.e(acStateKey), 1);
+        let proofId = UpdateStatusCircuit.PROOF_ID_TYPE_DEPOSIT;
+        let inputs = await UpdateStatusCircuit.createJoinSplitInput(
+            accountKey,
+            signingKey,
+            worldState,
+            acStateKey,
+            proofId,
+            aliasHash,
+            assetId,
+            assetId,
+            10n,
+            signingKey.pubKey,
+            10n,
+            accountKey.pubKey,
+            [],
+            accountRequired
+        );
+        for (const input of inputs) {
+            await utils.executeCircuit(circuit, input.toCircuitInput(babyJub));
+        }
+        console.log("test send tx")
+        let confirmedNote: Note[] = [];
+        for (const inp of inputs) {
+            inp.outputNotes[0].index = 10; // FIXME update index
+            inp.outputNotes[1].index = 10;
+            confirmedNote.push(inp.outputNotes[0]); // after depositing, all balance becomes private value
+            confirmedNote.push(inp.outputNotes[1]);
+        }
 
-    //     // create a send proof
-    //     let noteReceiver = await (new SigningKey()).newKey(undefined);
-    //     proofId = UpdateStatusCircuit.PROOF_ID_TYPE_SEND;
-    //     let inputs2 = await UpdateStatusCircuit.createJoinSplitInput(
-    //         accountKey,
-    //         signingKey,
-    //         worldState,
-    //         acStateKey,
-    //         proofId,
-    //         aliasHash,
-    //         assetId,
-    //         0,
-    //         0n, // public value
-    //         undefined, // public owner
-    //         5n, // receiver private value
-    //         noteReceiver.pubKey,
-    //         confirmedNote,
-    //         accountRequired
-    //     );
-    //     for (const input of inputs2) {
-    //         await utils.executeCircuit(circuit, input.toCircuitInput(babyJub));
-    //     }
-    // })
+        // create a send proof
+        let noteReceiver = await (new SigningKey()).newKey(undefined);
+        proofId = UpdateStatusCircuit.PROOF_ID_TYPE_SEND;
+        let inputs2 = await UpdateStatusCircuit.createJoinSplitInput(
+            accountKey,
+            signingKey,
+            worldState,
+            acStateKey,
+            proofId,
+            aliasHash,
+            assetId,
+            0,
+            0n, // public value
+            undefined, // public owner
+            5n, // receiver private value
+            noteReceiver.pubKey,
+            confirmedNote,
+            accountRequired
+        );
+        for (const input of inputs2) {
+            await utils.executeCircuit(circuit, input.toCircuitInput(babyJub));
+        }
+    })
 
     it("update_state verify proof test", async () => {
         let inputJson = path.join(__dirname, "..", "circuits/main_update_state.input.json");
