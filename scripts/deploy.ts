730667dca36d2bbb0d28f3abd2040692c640903b
eigmax <stephen@ieigen.com>
2023-04-05 00:50:45 +0800

    feat: add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 import { RollupHelper } from "../test/rollup.helper";
 import { deploySpongePoseidon, deployPoseidons, deployPoseidonFacade } from "../test/deploy_poseidons.util";
 
-const contractAddress = ".contract.json";
+const contractFile = ".contract.json";
 const fs = require("fs");
 
 const deploy = async() => {
@@ -11,13 +11,17 @@ const deploy = async() => {
         admin,
         [2, 3, 6]
     );
+    let contractJson = new Map<string, string>();
     console.log("Using account ", admin.address);
+    contractJson.set("admin", admin.address);
 
     let spongePoseidon = await deploySpongePoseidon(poseidonContracts[2].address);
+    contractJson.set("spongePoseidon", spongePoseidon.address);
     let factoryTR = await ethers.getContractFactory("TokenRegistry");
     let tokenRegistry = await factoryTR.deploy(admin.address)
     await tokenRegistry.deployed()
     console.log("tokenRegistry deployed to:", tokenRegistry.address);
+    contractJson.set("tokenRegistry", tokenRegistry.address);
 
     let factoryR = await ethers.getContractFactory(
         "Rollup",
@@ -27,6 +31,9 @@ const deploy = async() => {
             }
         }
     );
+    contractJson.set("poseidon2", poseidonContracts[0].address);
+    contractJson.set("poseidon3", poseidonContracts[1].address);
+    contractJson.set("poseidon6", poseidonContracts[2].address);
     let rollup = await factoryR.deploy(
         poseidonContracts[0].address,
         poseidonContracts[1].address,
@@ -34,21 +41,21 @@ const deploy = async() => {
     );
     await rollup.deployed();
     console.log("rollup deployed to:", rollup.address);
-}
+    contractJson.set("rollup", rollup.address);
 
-
-const deployTestToken = async() => {
-    let [admin] = await ethers.getSigners();
     let factoryTT = await ethers.getContractFactory("TestToken");
     let testToken = await factoryTT.connect(admin).deploy();
     await testToken.deployed();
     console.log("TestToken deployed to:", testToken.address);
+    contractJson.set("testToken", testToken.address);
+
+    console.log(contractJson);
+    fs.writeFileSync(contractFile, JSON.stringify(Object.fromEntries(contractJson)))
 }
 
 
 async function main() {
     await deploy();
-    await deployTestToken();
     console.log("Done");
 }
 
