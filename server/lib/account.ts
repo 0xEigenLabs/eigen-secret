3f1f03ef53d610606cfe470fa00940ecf076b08e
Stephen <81497928+eigmax@users.noreply.github.com>
2023-04-26 22:06:10 +0800

    feat: account store (#101)
    * feat: account store
    
    * feat: account store

--- server/lib/account.ts
+++ server/lib/account.ts
@@ -16,12 +16,8 @@ AccountModel.init({
         type: DataTypes.STRING,
         allowNull: false
     },
-    ethAddress2: { // backup
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    ethAddress3: { // backup no2
-        type: DataTypes.STRING,
+    secretAccount: {
+        type: DataTypes.TEXT,
         allowNull: false
     }
 }, {
@@ -30,12 +26,43 @@ AccountModel.init({
     modelName: "AccountModel" // We need to choose the model name
 });
 
+export async function getAccount(req: any, res: any) {
+    const alias = req.body.alias;
+    const ethAddress = req.body.ethAddress;
+    const timestamp = req.body.timestamp;
+    const rawMessage = req.body.message;
+    const hexSignature = req.body.hexSignature;
+    if (
+        !utils.hasValue(alias) ||
+        !utils.hasValue(hexSignature) ||
+        !utils.hasValue(rawMessage) ||
+        !utils.hasValue(timestamp) ||
+        !utils.hasValue(ethAddress)) {
+        consola.log(alias, hexSignature, rawMessage, timestamp, ethAddress)
+        return res.json(utils.err(utils.ErrCode.InvalidInput, "missing input"));
+    }
+
+    let validAdddr = await utils.verifyEOASignature(rawMessage, hexSignature, ethAddress, alias, timestamp);
+    if (!validAdddr) {
+        return res.json(utils.err(utils.ErrCode.InvalidInput, "Invalid EOA address"));
+    }
+    let result: any;
+    try {
+        result = await AccountModel.findAll({ where: { alias: alias, ethAddress: ethAddress } });
+    } catch (err: any) {
+        consola.log(err)
+        return res.json(utils.err(utils.ErrCode.DBCreateError, err.toString()));
+    }
+    return res.json(utils.succ(result));
+}
+
 // add new key
 export async function createAccount(req: any, res: any) {
     const alias = req.body.alias;
-    const ethAddress = req.params.ethAddress;
+    const ethAddress = req.body.ethAddress;
     const timestamp = req.body.timestamp;
     const rawMessage = req.body.message;
+    const secretAccount = req.body.secretAccount;
     const hexSignature = req.body.hexSignature;
     if (
         !utils.hasValue(alias) ||
@@ -66,7 +93,7 @@ export async function createAccount(req: any, res: any) {
     try {
         let insertResult = await upsert(
             AccountModel,
-            { alias, ethAddress, ethAddress2: "", ethAddress3: "" }, // new item
+            { alias, ethAddress, secretAccount }, // new item
             { alias, ethAddress }, // condition
             { transaction }
 
--- test/api_account.test.ts
+++ test/api_account.test.ts
@@ -1,27 +1,50 @@
 const request = require("supertest");
 import { ethers } from "ethers";
-
+const createBlakeHash = require("blake-hash");
 import app from "../server/dist/service";
 import * as utils from "@eigen-secret/core/dist-node/utils";
 import { expect, assert } from "chai";
+import { SigningKey, SecretAccount } from "@eigen-secret/core/dist-node/account";
+const { buildEddsa } = require("circomlibjs");
+
 /* globals describe, before, it */
 describe("POST /accounts", function() {
+    let alias = "eigen.eth";
+    let newEOAAccount: any;
+    let secretAccount: any;
+    let eddsa: any;
+    let key:any;
     before(async () => {
-        let newEOAAccount = await ethers.Wallet.createRandom();
+        newEOAAccount = await ethers.Wallet.createRandom();
+        eddsa = await buildEddsa();
         let timestamp = Math.floor(Date.now()/1000).toString();
-        let alias = "eigen.eth";
-        const signature = await utils.signEOASignature(newEOAAccount,
+        let signingKey = new SigningKey(eddsa);
+        let accountKey = new SigningKey(eddsa);
+        let newSigningKey1 = new SigningKey(eddsa);
+        let newSigningKey2 = new SigningKey(eddsa);
+        secretAccount = new SecretAccount(
+            alias, accountKey, signingKey, accountKey, newSigningKey1, newSigningKey2
+        );
+
+        const signature = await utils.signEOASignature(
+            newEOAAccount,
             utils.rawMessage,
             newEOAAccount.address,
             alias,
-            timestamp);
+            timestamp
+        );
+
+        let password = "12121";
+        key = createBlakeHash("blake256").update(Buffer.from(password)).digest();
         const response = await request(app)
-        .post("/accounts/" + newEOAAccount.address)
+        .post("/accounts/create")
         .send({
             alias: alias,
+            ethAddress: newEOAAccount.address,
             message: utils.rawMessage,
             timestamp: timestamp,
-            hexSignature: signature
+            hexSignature: signature,
+            secretAccount: secretAccount.serialize(key)
         })
         .set("Accept", "application/json");
 
@@ -32,14 +55,37 @@ describe("POST /accounts", function() {
         expect(response.body.data["ethAddress"]).to.eq(newEOAAccount.address);
     });
 
-    it.skip("responds with json", async () => {
+    it("responds with json", async () => {
+        let timestamp = Math.floor(Date.now()/1000).toString();
+        const signature = await utils.signEOASignature(
+            newEOAAccount,
+            utils.rawMessage,
+            newEOAAccount.address,
+            alias,
+            timestamp
+        );
         const response = await request(app)
-        .get("/accounts/alice.eth")
+        .post("/accounts/get")
+        .send({
+            alias: alias,
+            ethAddress: newEOAAccount.address,
+            message: utils.rawMessage,
+            timestamp: timestamp,
+            hexSignature: signature
+        })
         .set("Accept", "application/json");
 
         expect(response.status).to.eq(200);
         expect(response.body.errno).to.eq(0);
-        console.log(response.body);
         assert(response.body.data[0].alias, "alice.eth")
+        let sa = SecretAccount.deserialize(eddsa, key, response.body.data[0].secretAccount);
+        assert(
+            sa.accountKey.pubKey.pubKey,
+            secretAccount.accountKey.pubKey.pubKey
+        );
+        assert(
+            sa.signingKey.pubKey.pubKey,
+            secretAccount.signingKey.pubKey.pubKey
+        );
     });
 });
