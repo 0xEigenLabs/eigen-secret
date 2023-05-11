3285e1c63f12a492c91df73f585fa188ef1e3c62
eigmax <stephen@ieigen.com>
2023-05-11 22:46:47 +0800

    fix: atomic operation

--- server/lib/app.ts
+++ server/lib/app.ts
@@ -9,22 +9,14 @@ import app from "./service";
 import consola from "consola";
 import express from "express";
 import path from "path";
-import http from "http";
-import { createHttpTerminator } from "http-terminator";
-import "./process";
 
 app.use("/public", express.static(path.resolve(__dirname, "../../circuits"), {
   maxAge: 3600000, // set cache time to 1 hour
   etag: true // enable ETag
 }));
 
-export const server = http.createServer(app);
-export const httpTerminator = createHttpTerminator({
-  server
-});
-
 const port = process.env.PORT || 3000;
-server.listen(port, function() {
+app.listen(port, function() {
   consola.log(`Listening on port ${port}`);
 });
 
