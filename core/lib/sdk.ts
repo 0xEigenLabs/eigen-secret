c9f3103ef3dd1b51897af4625d91c654c0e32b21
Stephen <81497928+eigmax@users.noreply.github.com>
2023-05-26 10:27:08 +0800

    chore: format value (#155)
    * chore: code polish
    
    * fix: format
    
    * fix: format

--- core/lib/sdk.ts
+++ core/lib/sdk.ts
@@ -1018,11 +1018,18 @@ export class SecretSDK {
         return this.curl("assets/create", data);
     }
 
+    parseValue(ctx: Context, value: string, assetId: number, decimals: number = 18) {
+        if (assetId === 1) {
+            decimals = 18;
+        }
+        return utils.parseUnits(value, decimals).toBigInt();
+    }
+
     formatValue(ctx: Context, value: string, assetId: number, decimals: number = 18) {
         if (assetId === 1) {
             decimals = 18;
         }
-        return utils.parseUnits(value, decimals);
+        return utils.formatUnits(value, decimals);
     }
 
     async approve(token: string, value: bigint) {
