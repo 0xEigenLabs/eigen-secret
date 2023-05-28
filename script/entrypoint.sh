ef9695f305e41aa34ccdd9fb9098b3d39b6dec6a
eigmax <stephen@ieigen.com>
2023-05-28 16:38:32 +0800

    chore: code polish

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-(
-    cd /app/eigen-secret/server/node_modules/sqlite3/ && npm i
-)
 
-forever ./server/dist/app.js
+cd /app/eigen-secret/server/node_modules/sqlite3/ && npm i
+cd /app/eigen-secret/server
+forever ./dist/app.js
 
