91ccfe82a12b44506b5c0301475141cb44e5da6b
eigmax <stephen@ieigen.com>
2023-05-29 15:04:43 +0800

    feat: compress

--- server/lib/app.ts
+++ server/lib/app.ts
@@ -9,14 +9,6 @@ import app from "./service";
 import consola from "consola";
 import express from "express";
 import path from "path";
-import compression, {filter} from "compression";
-
-const shouldCompress = (req: any, res: any) => {
-    const contentType = req.getHeader("Content-Type");
-    const originFilter = filter(req, res);
-    return (contentType === "application/octet-stream" || contentType === "application/wasm") || originFilter;
-};
-app.use(compression({ filter: shouldCompress }));
 
 app.use("/public", express.static(path.resolve(__dirname, "../../circuits"), {
   maxAge: 3600000, // set cache time to 1 hour
