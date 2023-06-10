501bb17ecae6323185eb9998a6f23530333afe88
ibmp33 <2285673866@qq.com>
2023-06-10 23:27:34 +0800

    chore: add proxy contract

--- test.sh
+++ test.sh
@@ -25,21 +25,21 @@ npx hardhat deposit --alias Alice --index 0 --value 10 --asset-id ${ASSET_ID} --
 
 npx hardhat get-balance --alias Alice --index 0 --asset-id ${ASSET_ID} --network $NETWORK
 
-npx hardhat create-account --alias Bob --index 1 --network $NETWORK
+npx hardhat create-account --alias Bob --index 2 --network $NETWORK
 
 npx hardhat send-l1 --alias Alice --asset-id ${ASSET_ID} --receiver 0x70997970C51812dc3A010C7d01b50e0d17dc79C8 --value 100 --network $NETWORK
 
-npx hardhat deposit --alias Bob --index 1 --value 10 --asset-id ${ASSET_ID} --network $NETWORK
-npx hardhat deposit --alias Bob --index 1 --value 10 --asset-id ${ASSET_ID} --network $NETWORK
-npx hardhat get-balance --alias Bob --index 1 --asset-id ${ASSET_ID} --network $NETWORK
+npx hardhat deposit --alias Bob --index 2 --value 10 --asset-id ${ASSET_ID} --network $NETWORK
+npx hardhat deposit --alias Bob --index 2 --value 10 --asset-id ${ASSET_ID} --network $NETWORK
+npx hardhat get-balance --alias Bob --index 2 --asset-id ${ASSET_ID} --network $NETWORK
 
 npx hardhat get-balance --alias Alice --index 0 --asset-id ${ASSET_ID} --network $NETWORK
-npx hardhat get-balance --alias Bob --index 1 --asset-id ${ASSET_ID} --network $NETWORK
+npx hardhat get-balance --alias Bob --index 2 --asset-id ${ASSET_ID} --network $NETWORK
 
 npx hardhat migrate-account --alias Alice --index 0 --network $NETWORK
 
 npx hardhat get-balance --alias Alice --index 0 --asset-id ${ASSET_ID} --network $NETWORK
-npx hardhat get-balance --alias Bob --index 1 --asset-id ${ASSET_ID} --network $NETWORK
+npx hardhat get-balance --alias Bob --index 2 --asset-id ${ASSET_ID} --network $NETWORK
 
 npx hardhat deposit --alias Alice --index 0 --value 11 --asset-id ${ASSET_ID} --network $NETWORK
 
