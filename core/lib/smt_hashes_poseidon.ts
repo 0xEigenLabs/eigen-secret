eb5a4008e5a01a5b514aa8ff2c4b5ae059015b9c
eigmax <stephen@ieigen.com>
2023-06-22 08:41:51 +0800

    chore: impl SMT

---
+++ core/lib/smt_hashes_poseidon.ts
@@ -0,0 +1,16 @@
+const { buildPoseidon } = require("circomlibjs");
+const { getCurveFromName } = require("ffjavascript-browser");
+
+export default async function getHashes() {
+    const bn128 = await getCurveFromName("bn128", true);
+    const poseidon = await buildPoseidon();
+    return {
+        hash0: function (left: any, right: any) {
+            return poseidon([left, right]);
+        },
+        hash1: function(key: any, value: any) {
+            return poseidon([key, value, bn128.Fr.one]);
+        },
+        F: bn128.Fr
+    }
+}
