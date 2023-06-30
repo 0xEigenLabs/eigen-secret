58f5bec83e9690c5a1928c808447ddfbf104feec
ibmp33 <2285673866@qq.com>
2023-07-01 01:31:03 +0800

    chore: test concurrency

--- tasks/transaction.ts
+++ tasks/transaction.ts
@@ -115,6 +115,37 @@ async function runWithdrawTask(alias: string, assetId: number, password: string,
   await sdk.submitProofs(ctx, proofAndPublicSignals.data);
 }
 
+async function getBalanceForUser(user: any, alias: string, password: string, assetId: number) {
+  let timestamp = Math.floor(Date.now()/1000).toString();
+  const signature = await signEOASignature(user, rawMessage, user.address, timestamp);
+  const ctx = new Context(
+    alias,
+    user.address,
+    rawMessage,
+    timestamp,
+    signature
+  );
+  const contractJson = require(defaultContractFile);
+  let secretSDK = await SecretSDK.initSDKFromAccount(
+      ctx, defaultServerEndpoint, password, user, contractJson, defaultCircuitPath, defaultContractABI
+  );
+  if (secretSDK.errno != ErrCode.Success) {
+    console.log("initSDKFromAccount failed: ", secretSDK);
+  }
+  let sdk: SecretSDK = secretSDK.data;
+  let balance = await sdk.getAllBalance(ctx);
+  if (balance.errno != ErrCode.Success) {
+    console.log("getAllBalance failed: ", balance);
+  }
+  console.log("L2 balance for", alias, JSON.stringify(balance.data));
+
+  assetId = Number(assetId);
+  let address = await sdk.getRegisteredToken(assetId);
+  console.log("Token Address for", alias, address);
+  return address;
+}
+
+
 task("deposit", "Deposit asset from L1 to L2")
   .addParam("alias", "user alias")
   .addParam("assetId", "asset id/token id")
@@ -154,40 +185,44 @@ task("withdraw", "Withdraw asset from L2 to L1")
     await runWithdrawTask(alias, assetId, password, value, user);
   })
 
+task("get-balances-multi", "Get multiple users' both L1 and L2 balance")
+  .addParam("accountNum", "number of accounts to use")
+  .addParam("assetId", "asset id")
+  .addParam("password", "password for key sealing", "<your password>")
+  .setAction(async ({ accountNum, assetId, password }, { ethers }) => {
+    let accounts = await ethers.getSigners();
+    accounts.splice(2, 1); // remove the third user (index 2) since the proxy contract is deployed using this account
+    accounts = accounts.slice(0, accountNum); // Use the first 'accountNum' accounts
+    let userAliases = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "George", "Hannah", "Ivy", "Jack"];
+    let balancePromises = accounts.map(async (account, i) => {
+      let userName = userAliases[i];
+      let address = await getBalanceForUser(account, userName, password, assetId);
+      let balanceL1: any;
+      if (assetId !== 1) {
+          let tokenIns = new ethers.Contract(
+              address,
+              defaultContractABI.testTokenContractABI,
+              account
+          );
+          balanceL1 = await tokenIns.balanceOf(account.address);
+      } else {
+          balanceL1 = await account.getBalance();
+      }
+      console.log("L1 balance for", userName, balanceL1.toString());
+    });
+
+    await Promise.all(balancePromises);
+  });
+
 task("get-balance", "Get user's both L1 and L2 balance")
   .addParam("alias", "user name", "Alice")
   .addParam("assetId", "asset id")
   .addParam("password", "password for key sealing", "<your password>")
   .addParam("index", "user index for test")
   .setAction(async ({ alias, assetId, password, index }, { ethers }) => {
-    let timestamp = Math.floor(Date.now()/1000).toString();
     let account = await ethers.getSigners();
     let user = account[index];
-    const signature = await signEOASignature(user, rawMessage, user.address, timestamp);
-    const ctx = new Context(
-      alias,
-      user.address,
-      rawMessage,
-      timestamp,
-      signature
-    );
-    const contractJson = require(defaultContractFile);
-    let secretSDK = await SecretSDK.initSDKFromAccount(
-        ctx, defaultServerEndpoint, password, user, contractJson, defaultCircuitPath, defaultContractABI
-    );
-    if (secretSDK.errno != ErrCode.Success) {
-      console.log("initSDKFromAccount failed: ", secretSDK);
-    }
-    let sdk: SecretSDK = secretSDK.data;
-    let balance = await sdk.getAllBalance(ctx);
-    if (balance.errno != ErrCode.Success) {
-      console.log("getAllBalance failed: ", balance);
-    }
-    console.log("L2 balance", JSON.stringify(balance.data));
-
-    assetId = Number(assetId);
-    let address = await sdk.getRegisteredToken(assetId);
-    console.log("Token Address", address);
+    let address = await getBalanceForUser(user, alias, password, assetId);
     let balanceL1: any;
     if (assetId !== 1) {
         let tokenIns = new ethers.Contract(
@@ -195,7 +230,6 @@ task("get-balance", "Get user's both L1 and L2 balance")
             defaultContractABI.testTokenContractABI,
             user
         );
-
         balanceL1 = await tokenIns.balanceOf(user.address);
     } else {
         balanceL1 = await user.getBalance();
@@ -237,72 +271,79 @@ task("get-transactions", "Get user's transactions")
     console.log("transactions", transactions.data);
 });
 
-task("depositall", "Deposit assets from multiple users")
+task("deposit-multi", "Deposit assets from multiple users")
+.addParam("accountNum", "select the number of test users from 3-10")
 .addParam("assetId", "asset id/token id")
+.addParam("password", "password for key sealing", "<your password>")
 .addParam("value", "amount of transaction")
-.setAction(async ({ assetId, value }, { ethers }) => {
-  let account = await ethers.getSigners();
-  let user0 = account[0];
-  let user1 = account[1];
-  let user2 = account[3];
-  let result = await Promise.allSettled([
-    runDepositTask("Alice", assetId, "<your password>", value, user0),
-    runDepositTask("Bob", assetId, "<your password>", value, user1),
-    runDepositTask("Charlie", assetId, "<your password>", value, user2)
-  ]);
+.setAction(async ({ accountNum, assetId, password, value }, { ethers }) => {
+  let accounts = await ethers.getSigners();
+  accounts.splice(2, 1); // remove the third user (index 2) since the proxy contract is deployed using this account
+  accounts = accounts.slice(0, accountNum);
+  let userAliases = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "George", "Hannah", "Ivy", "Jack"];
+  let taskPromises = accounts.map((account, i) => {
+    let userName = userAliases[i];
+    return runDepositTask(userName, assetId, password, value, account);
+  });
+  let result = await Promise.allSettled(taskPromises);
   console.log(JSON.stringify(result))
 });
 
-task("sendall", "Collaborative asset transfer by multiple users")
+task("send-multi", "Collaborative asset transfer by multiple users")
+.addParam("accountNum", "select the number of test users from 3-10")
 .addParam("assetId", "asset id/token id")
 .addParam("password", "password for key sealing", "<your password>")
 .addParam("value", "amount of transaction")
 .addParam("receiverAlias", "receiver_alias use for test", __DEFAULT_ALIAS__)
-.setAction(async ({ assetId, password, value, receiverAlias }, { ethers }) => {
-  let account = await ethers.getSigners();
-  let user0 = account[0];
-  let user1 = account[1];
-  let user2 = account[3];
-  // const eddsa = await buildEddsa();
-  let timestamp = Math.floor(Date.now()/1000).toString();
-  const signature0 = await signEOASignature(user0, rawMessage, user0.address, timestamp);
-  const ctx0 = new Context("Alice", user0.address, rawMessage, timestamp, signature0);
-  const contractJson = require(defaultContractFile);
-  let secretSDK0 = await SecretSDK.initSDKFromAccount(
-    ctx0, defaultServerEndpoint, password, user0, contractJson, defaultCircuitPath, defaultContractABI
-  );
-  if (secretSDK0.errno != ErrCode.Success) {
-    console.log("initSDKFromAccount failed: ", secretSDK0);
+.setAction(async ({ accountNum, assetId, password, value, receiverAlias }, { ethers }) => {
+  let accounts = await ethers.getSigners();
+  accounts.splice(2, 1); // remove the third user (index 2) since the proxy contract is deployed using this account
+  accounts = accounts.slice(0, accountNum);
+  let userAliases = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "George", "Hannah", "Ivy", "Jack"];
+  async function setupUser(user: any, userName: string) {
+    let timestamp = Math.floor(Date.now()/1000).toString();
+    const signature = await signEOASignature(user, rawMessage, user.address, timestamp);
+    const ctx = new Context(userName, user.address, rawMessage, timestamp, signature);
+    const contractJson = require(defaultContractFile);
+    let secretSDK = await SecretSDK.initSDKFromAccount(
+      ctx, defaultServerEndpoint, password, user, contractJson, defaultCircuitPath, defaultContractABI
+    );
+    if (secretSDK.errno != ErrCode.Success) {
+      console.log("initSDKFromAccount failed: ", secretSDK);
+    }
+    let accountKeyPubKey = secretSDK.data.account.accountKey.pubKey.pubKey;
+    return { user, userName, accountKeyPubKey };
   }
-  let accountKeyPubKey0 = secretSDK0.data.account.accountKey.pubKey.pubKey;
 
-  const signature1 = await signEOASignature(user1, rawMessage, user1.address, timestamp);
-  const ctx1 = new Context("Bob", user1.address, rawMessage, timestamp, signature1);
-  let secretSDK1 = await SecretSDK.initSDKFromAccount(
-    ctx1, defaultServerEndpoint, password, user1, contractJson, defaultCircuitPath, defaultContractABI
-  );
-  if (secretSDK1.errno != ErrCode.Success) {
-    console.log("initSDKFromAccount failed: ", secretSDK1);
-  }
-  let accountKeyPubKey1 = secretSDK1.data.account.accountKey.pubKey.pubKey;
+  let setupPromises = accounts.map((account, i) => {
+    let userName = userAliases[i];
+    return setupUser(account, userName);
+  });
+  let setups = await Promise.all(setupPromises);
 
-  const signature2 = await signEOASignature(user2, rawMessage, user2.address, timestamp);
-  const ctx2 = new Context("Charlie", user2.address, rawMessage, timestamp, signature2);
-  let secretSDK2 = await SecretSDK.initSDKFromAccount(
-    ctx2, defaultServerEndpoint, password, user2, contractJson, defaultCircuitPath, defaultContractABI
-  );
-  if (secretSDK2.errno != ErrCode.Success) {
-    console.log("initSDKFromAccount failed: ", secretSDK2);
-  }
-  let accountKeyPubKey2 = secretSDK2.data.account.accountKey.pubKey.pubKey;
+  let taskPromises = setups.map((setup, i) => {
+    let nextSetup = setups[(i + 1) % setups.length]; // get the next user's setup
+    return runSendTask(setup.userName, assetId, password, value, setup.user, nextSetup.accountKeyPubKey, receiverAlias);
+  });
+  let result = await Promise.allSettled(taskPromises);
 
-  let result = await Promise.allSettled([
-    runSendTask("Alice", assetId, "<your password>", value, user0, accountKeyPubKey1, receiverAlias),
-    runSendTask("Bob", assetId, "<your password>", value, user1, accountKeyPubKey2, receiverAlias),
-    runSendTask("Charlie", assetId, "<your password>", value, user2, accountKeyPubKey0, receiverAlias),
-    runWithdrawTask("Alice", assetId, "<your password>", value, user0),
-    runWithdrawTask("Bob", assetId, "<your password>", value, user1),
-    runWithdrawTask("Charlie", assetId, "<your password>", value, user2)
-  ]);
+  console.log(JSON.stringify(result))
+});
+
+task("withdraw-multi", "Withdraw assets from multiple users")
+.addParam("accountNum", "select the number of test users from 3-10")
+.addParam("assetId", "asset id/token id")
+.addParam("password", "password for key sealing", "<your password>")
+.addParam("value", "amount of transaction")
+.setAction(async ({ accountNum, assetId, password, value }, { ethers }) => {
+  let accounts = await ethers.getSigners();
+  accounts.splice(2, 1); // remove the third user (index 2) since the proxy contract is deployed using this account
+  accounts = accounts.slice(0, accountNum);
+  let userAliases = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "George", "Hannah", "Ivy", "Jack"];
+  let taskPromises = accounts.map((account, i) => {
+    let userName = userAliases[i];
+    return runWithdrawTask(userName, assetId, password, value, account);
+  });
+  let result = await Promise.allSettled(taskPromises);
   console.log(JSON.stringify(result))
 });
